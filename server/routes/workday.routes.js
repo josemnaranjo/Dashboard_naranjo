2d499dfff0686dca1d4a85dfb57dd37aca13297e
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-08-22 10:17:33 -0400

    feat: ausente ruta

--- server/routes/workday.routes.js
+++ server/routes/workday.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { checkIn, checkOut } from "../controllers/workday.controller.js";
+import {
+  checkIn,
+  checkOut,
+  setAbsent,
+} from "../controllers/workday.controller.js";
 
 const router = Router();
 
 router.post("/check-in/:date", checkIn);
 router.put("/check-out/:date", checkOut);
+router.post("/set-absent/:date", setAbsent);
 
 export default router;
